b01d5443fe5bed7a69e4b400aa9cb593f8fab3cd
ngxc <ngoc.shintrann@gmail.com>
2026-07-01 19:03:34 +0700

    chore(deps): update dependencies and configure nextjs settings

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,11 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {/* config options here */};
+const nextConfig: NextConfig = {
+  reactStrictMode: true,
+  poweredByHeader: false,
+  images: {
+    formats: ["image/avif", "image/webp"],
+  },
+};
 
 export default nextConfig;
